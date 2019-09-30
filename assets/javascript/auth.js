af11806f3e50e26c0245d081419d49b674d22bf7
Bren <brennenbates2859@gmail.com>
2019-09-30 14:15:14 -0600

    Update auth.js
    added event listener for auth state changes to console log the user logged in or out.

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,62 +1,93 @@
-    
-    $(document).ready(function () { 
-    
-    // signup 
+    $(document).ready(function () {
 
-    const signUpForm = document.querySelector('#signUpForm');
+        //listen for auth status changes
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                console.log('user logged in: ', user);
+            } else { 
+                console.log('user logged out');
+            }
 
-
-    $('#signUpSubmit').on('click', function(event) { 
-    event.preventDefault();
-    let email = $('#signup-email').val().trim();
-    let password = $('#signup-password').val().trim();
-    console.log(email, password);
-
-    // sign up the user using the auth method of createUserWithEmailAndPassword
-    auth.createUserWithEmailAndPassword(email,password).then(cred => { 
-        console.log(cred.user);
-        const modal = $('#modalSignUp').modal('hide');
-        signUpForm.reset();
-        // signUpForm.reset();
-    });
-
-    });
-
-
+        });
         
-});
+        
+        // signup a new user
 
-   
-
-
-
+        const signUpForm = document.querySelector('#signUpForm');
 
 
+        $('#signUpSubmit').on('click', function (event) {
+            event.preventDefault();
+            let email = $('#signup-email').val().trim();
+            let password = $('#signup-password').val().trim();
+            console.log(email, password);
+
+            // sign up the user using the auth method of createUserWithEmailAndPassword
+            auth.createUserWithEmailAndPassword(email, password).then(cred => {
+                
+                let modal = $('#modalSignUp').modal('hide');
+                signUpForm.reset();
+
+            });
+        });
 
 
- 
-    //////////////////////////////////////////
-    //User Authentication with Firebase SDK //
-    //////////////////////////////////////////
-    
+        //log out the user when logout button is clicked 
+        $('#logOut').on('click', function (event) {
+            event.preventDefault();
+           
+            // log out the user 
+            auth.signOut();
+
+        });
+
+
+          //log in the user when login button is clicked and info input
+
+          const logInForm = document.querySelector("#logInForm")
+          $('#logInSubmit').on('click', function (event) {
+            event.preventDefault();
+            let email = $('#login-email').val().trim();
+            let password = $('#login-password').val().trim();
+            
+            auth.signInWithEmailAndPassword(email, password).then(cred => {
+                
+                //close the log in modal and reset the form
+                let modal = $('#modalLogIn').modal('hide');
+                logInForm.reset();
+            });
+           
+          
+
+        });
+
+
+    });
+
+
+
+
+
+
+
     //Create an instance of the Google provider object 
-    
+
     // var provider = new firebase.auth.GoogleAuthProvider();
     // console.log(provider);
-    
+
     //Authenticate with Firebase using the Google provider object. You can prompt your users to sign in with their Google Accounts either by opening a pop-up window
     //or by redirecting to the sign-in page. The redirect method is preferred on mobile devices.
-    
+
     //Signing in with sign-in page redirect
-    
+
     // $('#logIn').on('click', function(event) {
-    
+
     // console.log('Hello World!');
-    
+
     // firebase.auth().signInWithRedirect(provider);
-    
+
     // //Retrieve the Google provider's OAuth token by calling getRedirectResult when your page loads: 
-    
+
     // firebase.auth().getRedirectResult().then(function(result) {
     //     if (result.credential) {
     //       // This gives you a Google Access Token. You can use it to access the Google API.
@@ -75,7 +106,6 @@
     //     var credential = error.credential;
     //     // This is also where you can catch and handle errors. For a list of error codes have a look at the Auth Reference Docs.  https://firebase.google.com/docs/reference/js/firebase.auth.Auth#getRedirectResult
     //   });
-    
-    
+
+
     // });
-    
