a6eaa9cd9603bb2e3cb5c75aef1e7d44820bb0aa
Bren <brennenbates2859@gmail.com>
2019-10-02 00:29:17 -0600

    Update auth.js
    Added query to get data from the users collection of the database and store it in a dataArray.

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -11,9 +11,10 @@
 
         const setUpUI = (user) => {
 
+
             if (user) {
                 
-                console.log(user);
+                
                 let userHTML = $('<div>');
                 
 
@@ -71,6 +72,23 @@
             }
 
         }
+
+
+        //Query all food truck data and push to dataArray
+
+        let dataArray = [];
+
+        db.collection("users").get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                dataArray.push(doc.data());
+                console.log('this is the data Array');
+                console.log(dataArray);
+            });
+        });
+
+        
+
 
         ////////////////////////////////
         // AUTH.JS section ///////////
@@ -151,7 +169,7 @@
             let password = $('#signup-password').val().trim();
             let name = $('#signup-displayname').val().trim();
 
-            console.log(email, password, name);
+            
 
             // sign up the user using the auth method of createUserWithEmailAndPassword
             auth.createUserWithEmailAndPassword(email, password).then(cred => {
@@ -182,7 +200,7 @@
 
         const logInForm = document.querySelector("#logInForm")
         $('#logInSubmit').on('click', function (event) {
-            console.log('hello');
+            
             event.preventDefault();
             let email = $('#login-email').val().trim();
             let password = $('#login-password').val().trim();
@@ -201,48 +219,3 @@
 
     });
 
-
-
-
-
-
-
-    //Create an instance of the Google provider object 
-
-    // var provider = new firebase.auth.GoogleAuthProvider();
-    // console.log(provider);
-
-    //Authenticate with Firebase using the Google provider object. You can prompt your users to sign in with their Google Accounts either by opening a pop-up window
-    //or by redirecting to the sign-in page. The redirect method is preferred on mobile devices.
-
-    //Signing in with sign-in page redirect
-
-    // $('#logIn').on('click', function(event) {
-
-    // console.log('Hello World!');
-
-    // firebase.auth().signInWithRedirect(provider);
-
-    // //Retrieve the Google provider's OAuth token by calling getRedirectResult when your page loads: 
-
-    // firebase.auth().getRedirectResult().then(function(result) {
-    //     if (result.credential) {
-    //       // This gives you a Google Access Token. You can use it to access the Google API.
-    //       var token = result.credential.accessToken;
-    //       // ...
-    //     }
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //   }).catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // This is also where you can catch and handle errors. For a list of error codes have a look at the Auth Reference Docs.  https://firebase.google.com/docs/reference/js/firebase.auth.Auth#getRedirectResult
-    //   });
-
-
-    // });
