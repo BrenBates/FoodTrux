3daa6b7a938486d62094dc80fbe357738310d158
Bren <brennenbates2859@gmail.com>
2019-10-02 00:02:24 -0600

    Update auth.js
    Troubleshooting firebase database to allow user collection to include location of the truck and the truck name (displayName)

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -10,57 +10,67 @@
         let loggedInLinks = document.querySelectorAll('.logged-in');
 
         const setUpUI = (user) => {
-          
+
             if (user) {
+                
+                console.log(user);
                 let userHTML = $('<div>');
-                userHTML.text(`Logged in as ${user.email}`);
+                
+
+                if(user.email) {
+                    userHTML.html(`Logged in as ${user.email} ` + '<br>'
+                    + `Current Location: ${user.Address}`
+                    );
+                } else {
+                    // userHTML.text(`Logged in as ${user.email}`);
+                }
 
                 //update account info
                 $('#accountInfo').empty().append(userHTML);
                 //toggle UI elements
-               
+
                 loggedOutLinks.forEach(item => item.style.display = 'none');
                 loggedInLinks.forEach(item => item.style.display = 'block');
-                
-            } else { 
+
+            } else {
                 //hide account info 
                 $('#accountInfo').empty();
                 //toggle UI elements 
                 loggedInLinks.forEach(item => item.style.display = 'none');
                 loggedOutLinks.forEach(item => item.style.display = 'block');
-                
+
             }
         }
 
         //Example of building on the DOM with data from database. 
-        
-        let setUpLocations = (data) => { 
 
-        //If there is data (since we're logged in) then put on the page necessary information.
-        if (data.length) {
+        let setUpLocations = (data) => {
 
-        let html='';
-        data.forEach(doc => {
-            const location = doc.data();
-            const li = `
+            //If there is data (since we're logged in) then put on the page necessary information.
+            if (data.length) {
+
+                let html = '';
+                data.forEach(doc => {
+                    const location = doc.data();
+                    const li = `
                 <li>
                     <div>${location.Address}</div>
                     <div>${location.City}</div>
                     <div>${location.State}</div>
                 </li>
             `;
-            html += li;
+                    html += li;
 
-        });
+                });
 
-        
-        $('.locations').html(html);
-        //If there is no data, show a message to login to see locations
-    } else { 
-        $('.locations').html('<h5 class = "center-align">Login to view locations</h5>')
-    }
 
-    }
+                $('.locations').html(html);
+                //If there is no data, show a message to login to see locations
+            } else {
+                $('.locations').html('<h5 class = "center-align">Login to view locations</h5>')
+            }
+
+        }
 
         ////////////////////////////////
         // AUTH.JS section ///////////
@@ -70,95 +80,121 @@
         auth.onAuthStateChanged(user => {
             db.collection('Locations').onSnapshot(snapshot => {
 
-            if (user) {
-                  //get a snapshot of the guides collection on firestore and run the setupGuides function on it
+                if (user) {
+                    //get a snapshot of the guides collection on firestore and run the setupGuides function on it
                     setUpLocations(snapshot.docs);
                     setUpUI(user);
-               
-                console.log('user logged in: ', user);
-            } else { 
-                setUpLocations([]);
-                setUpUI();
-                console.log('user logged out');
-            }
 
+                    console.log('user logged in: ', user);
+                } else {
+                    setUpLocations([]);
+                    setUpUI();
+                    console.log('user logged out');
+                }
+
+            }, err => {
+                console.log(err.message)
             });
         });
 
-        // Create a new location for the food truck
+        // Create a new location for the food truck.  Store the data in locations and users collections.  This will allow us to pin where that food truck is
+        // but also keep track of popular food truck locations.
 
         let createLocation = document.querySelector('#newLocationForm');
 
-        $('#newLocationSubmit').on('click', function(event) { 
+        $('#newLocationSubmit').on('click', function (event) {
             event.preventDefault();
 
-            db.collection('Locations').add({
-                Address: $('#location-address').val().trim(),
-                City: $('#location-city').val().trim(),
-                State: $('#location-state').val().trim()
-            }).then(() => {
-                let modal = $('#modalNewLocation').modal('hide');
-                createLocation.reset();
-            }).catch(err => {
-                console.log(err.message);
-            });
 
+            let user = firebase.auth().currentUser;
+
+            if (user) {
+                user.Address = $('#location-address').val().trim();
+                user.City = $('#location-city').val().trim();
+                user.State = $('#location-state').val().trim();
+
+                db.collection('users').doc(user.uid).update({
+                    Address: user.Address,
+                    City: user.City,
+                    State: user.State
+                });
+            }
+
+            let modal = $('#modalNewLocation').modal('hide');
+            createLocation.reset();
+
+
+            // db.collection('Locations').add({
+            //     Address: $('#location-address').val().trim(),
+            //     City: $('#location-city').val().trim(),
+            //     State: $('#location-state').val().trim()
+            // }).then(() => {
+            //     let modal = $('#modalNewLocation').modal('hide');
+            //     createLocation.reset();
+            // }).catch(err => {
+            //     console.log(err.message);
+            // });
+
+               
         });
 
-        
+
         // signup a new user
 
         const signInForm = document.querySelector("#signUpForm");
 
         $('#submitSignUp').on('click', function (event) {
-            console.log('hello');
+            
             event.preventDefault();
-           
+
             let email = $('#signup-email').val().trim();
             let password = $('#signup-password').val().trim();
             let name = $('#signup-displayname').val().trim();
 
-            console.log(email, password,name);
+            console.log(email, password, name);
 
             // sign up the user using the auth method of createUserWithEmailAndPassword
             auth.createUserWithEmailAndPassword(email, password).then(cred => {
 
-                    return db.collection('users').doc(cred.user.uid).set({
-                        displayName: name
-                    });
+                let user = firebase.auth().currentUser;
+
+                return db.collection('users').doc(user.uid).set({
+                    displayName: name
+                });
             });
+
             let modal = $('#modalSignUp').modal('hide');
-                signUpForm.reset();
+            signUpForm.reset();
         });
 
 
         //log out the user when logout button is clicked 
         $('#logOut').on('click', function (event) {
             event.preventDefault();
-           
+
             // log out the user 
             auth.signOut();
 
         });
 
 
-          //log in the user when login button is clicked and info input
+        //log in the user when login button is clicked and info input
 
-          const logInForm = document.querySelector("#logInForm")
-          $('#logInSubmit').on('click', function (event) {
+        const logInForm = document.querySelector("#logInForm")
+        $('#logInSubmit').on('click', function (event) {
             console.log('hello');
             event.preventDefault();
             let email = $('#login-email').val().trim();
             let password = $('#login-password').val().trim();
-            
+
             auth.signInWithEmailAndPassword(email, password).then(cred => {
-                
+
                 //close the log in modal and reset the form
                 let modal = $('#modalLogIn').modal('hide');
                 logInForm.reset();
             });
-           
-          
+
+
 
         });
 
