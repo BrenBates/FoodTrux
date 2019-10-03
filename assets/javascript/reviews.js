7051644d2755e1b3453875ad91972c26e74ec51b
myles-dewaal <mylesdewaal@hotmail.com>
2019-10-02 20:11:49 -0600

    finalized url for Yelps API

---
+++ assets/javascript/reviews.js
@@ -0,0 +1,18 @@
+var cors = 'https://cors-anywhere.herokuapp.com/';
+var queryURL = "https://api.yelp.com/v3/businesses/"
+var input = $(".testInput").text()
+var city = $(".city").text() + "/reviews";
+var parsedCity = city.toLocaleLowerCase().split(" ").join("-");
+var parsedInput = input.toLocaleLowerCase().split(" ").join("-");
+$.ajax({
+    url: cors + queryURL + parsedInput + "-" + parsedCity,
+    method: "GET",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer WS8n-Flb5OtfN8lYl8iyGgwC6x0wUZtgY26RugjGZD32PMwsgFz87eRCGk5KmZK5i0nNrKVGCG0-8vxKjBsJhRYxD_P4IC95wx2ivc8mteChGdUyIJIk3jaQqamSXXYx"
+    },
+}).then(res => {
+    console.log('res', res)
+}).catch(err => {
+    console.log('err', err)
+})
