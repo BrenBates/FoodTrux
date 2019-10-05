770ee109795a0b7b62c273be67e006b5b140e0b5
Myles Dewaal <52334937+myles-dewaal@users.noreply.github.com>
2019-10-05 11:24:42 -0600

    Update reviews.js
    Testing reviews

--- assets/javascript/reviews.js
+++ assets/javascript/reviews.js
@@ -1,33 +1,11 @@
-var cors = 'https://cors-anywhere.herokuapp.com/';
-var queryURL = "https://api.yelp.com/v3/businesses/"
-var input = $(".testInput").text()
-var city = $(".city").text() + "/reviews";
-var parsedCity = city.toLocaleLowerCase().split(" ").join("-");
-var parsedInput = input.toLocaleLowerCase().split(" ").join("-");
-$.ajax({
-    url: cors + queryURL + parsedInput + "-" + parsedCity,
-    method: "GET",
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Authorization": "Bearer WS8n-Flb5OtfN8lYl8iyGgwC6x0wUZtgY26RugjGZD32PMwsgFz87eRCGk5KmZK5i0nNrKVGCG0-8vxKjBsJhRYxD_P4IC95wx2ivc8mteChGdUyIJIk3jaQqamSXXYx"
-    },
-}).then(res => {
-    console.log('res', res)
-}).catch(err => {
-    console.log('err', err)
-})
-var cors = 'https://cors-anywhere.herokuapp.com/';
-var queryURL = "https://api.yelp.com/v3/businesses/search?term=the-red-food-truck&location=84115"
-
-$.ajax({
-    url: cors + queryURL,
-    method: "GET",
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Authorization": "Bearer WS8n-Flb5OtfN8lYl8iyGgwC6x0wUZtgY26RugjGZD32PMwsgFz87eRCGk5KmZK5i0nNrKVGCG0-8vxKjBsJhRYxD_P4IC95wx2ivc8mteChGdUyIJIk3jaQqamSXXYx"
-    },
-}).then(res => {
-    console.log('res', res)
-}).catch(err => {
-    console.log('err', err)
-})
+var config = {
+    apiKey: "AIzaSyAcvshv4oMqKChcOEg0EpF6zn5qPqEqjpo",
+    authDomain: "fuudtruckz.firebaseapp.com",
+    databaseURL: "https://fuudtruckz.firebaseio.com",
+    projectId: "fuudtruckz",
+    storageBucket: "fuudtruckz.appspot.com",
+    messagingSenderId: "422049961079",
+    appId: "1:422049961079:web:6795965fcaadf1bc3ae272"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
