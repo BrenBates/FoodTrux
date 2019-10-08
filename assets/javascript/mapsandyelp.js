de1af33cb7e0784031455aacc7bb66ad7ce21405
Myles Dewaal <52334937+myles-dewaal@users.noreply.github.com>
2019-10-07 19:21:50 -0600

    Update mapsandyelp.js
    Added reviews to the DOM

--- assets/javascript/mapsandyelp.js
+++ assets/javascript/mapsandyelp.js
@@ -1,50 +1,50 @@
-    let map, infoWindow, marker; 
-    let locations = []; 
-    dataArray = [];
-    let foodTruck;
-    let cors = 'https://cors-anywhere.herokuapp.com/';
-    let queryURL = "https://api.yelp.com/v3/businesses/"
-    let city ;
-    let parsedFoodTruck;
-    let parsedCity;
+let map, infoWindow, marker;
+let locations = [];
+dataArray = [];
+let foodTruck;
+let cors = 'https://cors-anywhere.herokuapp.com/';
+let queryURL = "https://api.yelp.com/v3/businesses/"
+let city;
+let parsedFoodTruck;
+let parsedCity;
 
 
-    //Nested the Yelp AJAX request inside the on click which is inside the google maps creation function.  This allowed us to only query our data base one time for the needed data.
+//Nested the Yelp AJAX request inside the on click which is inside the google maps creation function.  This allowed us to only query our data base one time for the needed data.
 
-    function createMap () { 
+function createMap() {
 
-    //Set up the basic map and center it on Salt Lake City for now.
+  //Set up the basic map and center it on Salt Lake City for now.
 
-      map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: new google.maps.LatLng(40.7761563, -111.8945562),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: new google.maps.LatLng(40.7761563, -111.8945562),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
-    //Query the Firebase database to pull in all users information and push it into our dataArray.
-      db.collection("users").get().then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-            // doc.data() is never undefined for query doc snapshots
-            dataArray.push(doc.data());
-            
+  //Query the Firebase database to pull in all users information and push it into our dataArray.
+  db.collection("users").get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      // doc.data() is never undefined for query doc snapshots
+      dataArray.push(doc.data());
 
-    //Looping through the data array and putting it into new formatting needed to create all of the markers on the google Map.
-    for (let i = 0; i < dataArray.length; i++) {  
 
-        
-      locations[i] = [dataArray[i].displayName, dataArray[i].Latitude, dataArray[i].Longitude, dataArray[i].zipCode, dataArray[i].yelpCity, i+1];
-  
-      infowindow = new google.maps.InfoWindow();
-  
-    //Create new Google maps marker
+      //Looping through the data array and putting it into new formatting needed to create all of the markers on the google Map.
+      for (let i = 0; i < dataArray.length; i++) {
+
+
+        locations[i] = [dataArray[i].displayName, dataArray[i].Latitude, dataArray[i].Longitude, dataArray[i].zipCode, dataArray[i].yelpCity, i + 1];
+
+        infowindow = new google.maps.InfoWindow();
+
+        //Create new Google maps marker
         marker = new google.maps.Marker({
           position: new google.maps.LatLng(locations[i][1], locations[i][2]),
           map: map
         });
-  
-    //On click function for the markers.  When you click on a marker it will query Yelp.
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-          return function() {
+
+        //On click function for the markers.  When you click on a marker it will query Yelp.
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+          return function () {
             infowindow.setContent(locations[i][0]);
             infowindow.open(map, marker);
 
@@ -59,19 +59,28 @@
               url: cors + queryURL + parsedFoodTruck + "-" + parsedCity,
               method: "GET",
               headers: {
-                  "Access-Control-Allow-Origin": "*",
-                  "Authorization": "Bearer WS8n-Flb5OtfN8lYl8iyGgwC6x0wUZtgY26RugjGZD32PMwsgFz87eRCGk5KmZK5i0nNrKVGCG0-8vxKjBsJhRYxD_P4IC95wx2ivc8mteChGdUyIJIk3jaQqamSXXYx"
+                "Access-Control-Allow-Origin": "*",
+                "Authorization": "Bearer WS8n-Flb5OtfN8lYl8iyGgwC6x0wUZtgY26RugjGZD32PMwsgFz87eRCGk5KmZK5i0nNrKVGCG0-8vxKjBsJhRYxD_P4IC95wx2ivc8mteChGdUyIJIk3jaQqamSXXYx"
               },
-          }).then(res => {
+            }).then(res => {
               console.log('res', res)
-          }).catch(err => {
+              $(".review-insert").empty().append("Rating: " + res.rating, "<br>" + "Price: " + res.price + "<br>");
+              for (var i = 0; i < res.photos.length; i++) {
+                var reviewImage = $("<img>");
+                reviewImage.attr("src", res.photos[i]);
+                reviewImage.addClass("review-image");
+                $(".review-insert").append(reviewImage);
+              }
+
+            }).catch(err => {
               console.log('err', err)
-          })
-          
+            })
+
           }
+
         })(marker, i));
       }
-        });
     });
+  });
 
 }
